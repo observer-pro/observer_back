e29d8b38cf1901d538e409a218d39096b5e8f490
bendenko-v <bendenko.v@gmail.com>
2023-11-09 15:12:58 +0100

    Ping timeout added

--- src/main.py
+++ src/main.py
@@ -24,7 +24,7 @@ from src.events.settings import send_settings
 from src.events.sharing import send_sharing_code, send_sharing_status, send_signal
 from src.events.utils import emit_log
 
-sio = socketio.AsyncServer(async_mode='asgi', cors_allowed_origins='*')
+sio = socketio.AsyncServer(async_mode='asgi', cors_allowed_origins='*', ping_timeout=90)
 app = socketio.ASGIApp(sio)
 
 
