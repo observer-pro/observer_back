8e25812678cf826019c8617ae77f6e74abf52a42
bendenko-v <bendenko.v@gmail.com>
2024-02-08 15:36:28 +0100

    Events are covered with tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -19,88 +19,17 @@ async def client():
     yield await client_generator().__anext__()
 
 
-def create_response_fixture(user, event_name, type_of_data='dict'):
-    response = {} if type_of_data == 'dict' else []
-
-    @user.on(event_name)
-    async def handle_event(data):
-        nonlocal response
-        if type_of_data == 'dict':
-            response.update(data)
-        else:
-            response.extend(data)
-
-    return response
-
-
 @pytest_asyncio.fixture
-async def room_update(host: AsyncClient):
-    return create_response_fixture(host, 'room/update')
+async def events(client, host):
+    all_events = {}
 
+    async def event_handler(event, data):
+        all_events[event] = data
 
-@pytest_asyncio.fixture
-async def room_join(client: AsyncClient):
-    return create_response_fixture(client, 'room/join')
+    client.on('*', event_handler)
+    host.on('*', event_handler)
 
-
-@pytest_asyncio.fixture
-async def room_closed(client: AsyncClient):
-    return create_response_fixture(client, 'room/closed')
-
-
-@pytest_asyncio.fixture
-async def settings(client: AsyncClient):
-    return create_response_fixture(client, 'settings')
-
-
-@pytest_asyncio.fixture
-async def steps_all(client: AsyncClient):
-    return create_response_fixture(client, 'steps/all', type_of_data='list')
-
-
-@pytest_asyncio.fixture
-async def steps_table(host: AsyncClient):
-    return create_response_fixture(host, 'steps/table', type_of_data='list')
-
-
-@pytest_asyncio.fixture
-async def steps_to_mentor(host: AsyncClient):
-    return create_response_fixture(host, 'steps/status/to_mentor')
-
-
-@pytest_asyncio.fixture
-async def steps_to_client(client: AsyncClient):
-    return create_response_fixture(client, 'steps/status/to_client')
-
-
-@pytest_asyncio.fixture
-async def message_to_client(client: AsyncClient):
-    return create_response_fixture(client, 'message/to_client')
-
-
-@pytest_asyncio.fixture
-async def message_to_mentor(host: AsyncClient):
-    return create_response_fixture(host, 'message/to_mentor')
-
-
-@pytest_asyncio.fixture
-async def get_user_messages(host: AsyncClient):
-    return create_response_fixture(host, 'message/user')
-
-
-@pytest_asyncio.fixture
-async def message(client: AsyncClient):
-    return create_response_fixture(client, 'message')
-
-
-@pytest_asyncio.fixture
-async def error(host: AsyncClient):
-    return create_response_fixture(host, 'error')
-
-
-@pytest_asyncio.fixture
-async def alerts(client: AsyncClient):
-    return create_response_fixture(client, 'alerts')
+    yield all_events
 
 
 class TestContext:
